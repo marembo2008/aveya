2c370fada76c06c802329dc2300244bc7abcdbb9
Marembo Ochieng <marembo2008@gmail.com>
2014-03-20 01:33:27 +0300

    jpa attribute converter - removing hibernate specific type

--- src/main/java/com/anosym/jflemax/jpa/converter/AmountJpaConverter.java
+++ src/main/java/com/anosym/jflemax/jpa/converter/AmountJpaConverter.java
@@ -7,25 +7,17 @@ package com.anosym.jflemax.jpa.converter;
 
 import com.anosym.utilities.Utility;
 import com.anosym.utilities.currency.Amount;
-import java.io.Serializable;
 import java.math.BigDecimal;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Types;
 import java.util.Currency;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import org.hibernate.HibernateException;
-import org.hibernate.engine.spi.SessionImplementor;
-import org.hibernate.usertype.UserType;
 
 /**
  *
  * @author marembo
  */
 @Converter(autoApply = true)
-public class AmountJpaConverter implements AttributeConverter<Amount, String>, UserType {
+public class AmountJpaConverter implements AttributeConverter<Amount, String> {
 
   @SuppressWarnings("null")
   public String convertToDatabaseColumn(Amount attribute) {
@@ -41,49 +33,4 @@ public class AmountJpaConverter implements AttributeConverter<Amount, String>, U
     BigDecimal value = new BigDecimal(parts[1]);
     return new Amount(value, c);
   }
-
-  public int[] sqlTypes() {
-    return new int[]{Types.VARCHAR};
-  }
-
-  public Class returnedClass() {
-    return Amount.class;
-  }
-
-  public boolean equals(Object x, Object y) throws HibernateException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public int hashCode(Object x) throws HibernateException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner) throws HibernateException, SQLException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public void nullSafeSet(PreparedStatement st, Object value, int index, SessionImplementor session) throws HibernateException, SQLException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public Object deepCopy(Object value) throws HibernateException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public boolean isMutable() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public Serializable disassemble(Object value) throws HibernateException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public Object assemble(Serializable cached, Object owner) throws HibernateException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  public Object replace(Object original, Object target, Object owner) throws HibernateException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
 }
